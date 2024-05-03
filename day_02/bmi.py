b14624b4745da395aefbf18fedc69e627512d9ff
rockchalkjay <rockchalkjay@gmail.com>
2024-05-03 14:09:40 -0600

    moving directories/files around

---
+++ day_02/bmi.py
@@ -0,0 +1,5 @@
+height = float(input("Enter height:\n"))
+weight = float(input("Enter weight:\n"))
+
+bmi = int(weight / height ** 2)
+print("BMI = " + str(bmi))
---
+++ day_02/weeks_left.py
@@ -0,0 +1,5 @@
+age = int(input("What's your age?\n"))
+
+weeks_left = (90 - age) * 52
+
+print(f"You have {weeks_left} weeks left in your life")
